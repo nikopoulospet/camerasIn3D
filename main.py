8d29252fe99e5a9b0339b96b561f165cb461069e
nikopoulospet <peter@nikopoulos.net>
2021-06-26 19:25:43 -0400

    add project files

---
+++ main.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import cv2
+
+from camera import Camera
+import structure
+import processor
+import features
+
+# Download images from http://www.robots.ox.ac.uk/~vgg/data/mview/
+
+
+def dino():
+    # Dino
+    img1 = cv2.imread('testsets/dinos/viff.003.ppm')
+    img2 = cv2.imread('testsets/dinos/viff.001.ppm')
+    pts1, pts2 = features.find_correspondence_points(img1, img2)
+    points1 = processor.cart2hom(pts1)
+    points2 = processor.cart2hom(pts2)
+
+    fig, ax = plt.subplots(1, 2)
+    ax[0].autoscale_view('tight')
+    ax[0].imshow(cv2.cvtColor(img1, cv2.COLOR_BGR2RGB))
+    ax[0].plot(points1[0], points1[1], 'r.')
+    ax[1].autoscale_view('tight')
+    ax[1].imshow(cv2.cvtColor(img2, cv2.COLOR_BGR2RGB))
+    ax[1].plot(points2[0], points2[1], 'r.')
+    fig.show()
+
+    height, width, ch = img1.shape
+    intrinsic = np.array([  # for dino
+        [2360, 0, width / 2],
+        [0, 2360, height / 2],
+        [0, 0, 1]])
+
+    return points1, points2, intrinsic
+
+
+points1, points2, intrinsic = dino()
+
+# Calculate essential matrix with 2d points.
+# Result will be up to a scale
+# First, normalize points
+points1n = np.dot(np.linalg.inv(intrinsic), points1)
+points2n = np.dot(np.linalg.inv(intrinsic), points2)
+
+#TODO
+# calculate essential matrix
+
+
+#print essential matrix
+
+
+#display of the cameras in 3D space 
